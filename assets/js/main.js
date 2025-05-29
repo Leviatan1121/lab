7413d6da35ce377b3ef616312a304985c62b6ed3
Levi's Hub <137499479+Leviatan1121@users.noreply.github.com>
2025-05-29 12:33:41 +0200

    First prototype

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+var data;
+const list = document.getElementById("list");
+async function main() {
+    const res = await fetch("assets/json/data.json");
+    data = await res.json();
+    data.forEach(project => {
+        const ctn = document.createElement("div");
+        ctn.classList.add("project");
+        // ctn.classList.add("project-ctn");
+        const name = document.createElement("h3");
+        name.innerHTML = project.name;
+        const desc = document.createElement("p");
+        desc.innerHTML = project.description;
+        //const img = document.createElement("img");
+        //img.src = project.image;
+        const link = document.createElement("a");
+        link.innerText = "View Project";
+        link.href = project.url;
+
+        const download = document.createElement("a");
+        download.innerText = "Download";
+        download.href = `https://cdn.levihub.dev/${project.file}`;
+
+        ctn.appendChild(name);
+        ctn.appendChild(desc);
+        //ctn.appendChild(img);
+        ctn.appendChild(link);
+        ctn.appendChild(download);
+        list.appendChild(ctn);
+    });
+}
+window.onload = () => main();
